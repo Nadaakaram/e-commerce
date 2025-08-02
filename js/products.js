236229af1635fd15698e7789ff76580776fc4b69
Nada Karam <nadakkaram@gmail.com>
2025-08-02 23:42:06 +0300

    responsive updates in home, cart

--- js/products.js
+++ js/products.js
@@ -72,8 +72,8 @@ document.getElementById("sort-filter").addEventListener("change", () => {
 
 document.getElementById("logo").addEventListener(("click"),()=>{
   window.location.href = "index.html";
-
 })
+
 
 function displayProducts(productsArr) {
   productList.innerHTML = "";
